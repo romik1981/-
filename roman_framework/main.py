aed4a60e9048947cabe104b865033c388abfca59
Roman <belyakov1981@mail.ru>
2023-07-29 22:00:48 +0300

    Lesson_2 Ready

--- roman_framework/main.py
+++ roman_framework/main.py
@@ -1,4 +1,8 @@
 
+from quopri import decodestring
+from framework_requests import GetRequests, PostRequests
+
+
 class PageNotFound404:
     def __call__(self, request):
         return '404 WHAT', '404 PAGE Not Found'
@@ -20,13 +24,45 @@ class Framework:
         if not path.endswith('/'):
             path = f'{path}/'
 
+        request = {}
+        # Получаем все данные запроса
+        method = environ['REQUEST_METHOD']
+        request['method'] = method
+
+        if method == 'POST':
+            data = PostRequests().get_request_params(environ)
+            request['data'] = Framework.decode_value(data)
+            print(data)
+            print(f'Нам пришёл post-запрос: {Framework.decode_value(data)}')
+            for k,v in data.items():
+                if k == 'message' :
+                    print(f'Нам пришло сообщение:'
+                          f' {Framework.decode_value(data)}')
+                    with open(f'message.csv', 'a+', encoding='utf-8') as fw:
+                        for v in Framework.decode_value(data).items():
+                            fw.writelines(v)
+        if method == 'GET':
+            request_params = GetRequests().get_request_params(environ)
+            request['request_params'] = Framework.decode_value(request_params)
+            print(f'Нам пришли GET-параметры:'
+                  f' {Framework.decode_value(request_params)}')
+        if method == 'MESSAGE':
+            request_params = GetRequests().get_request_params(environ)
+            request['request_params'] = Framework.decode_value(request_params)
+            print(f'Нам пришло сообщение:'
+                  f' {Framework.decode_value(request_params)}')
+            with open(f'message.csv', 'w+', encoding='utf-8') as fw:
+                for k,v in Framework.decode_value(request_params):
+                    fw.writelines(k,v)
+
+
         # находим нужный контроллер
         # отработка паттерна page controller
         if path in self.routes_lst:
             view = self.routes_lst[path]
         else:
             view = PageNotFound404()
-        request = {}
+
         # наполняем словарь request элементами
         # этот словарь получат все контроллеры
         # отработка паттерна front controller
@@ -36,3 +72,12 @@ class Framework:
         code, body = view(request)
         start_response(code, [('Content-Type', 'text/html')])
         return [body.encode('utf-8')]
+
+    @staticmethod
+    def decode_value(data):
+        new_data = {}
+        for k, v in data.items():
+            val = bytes(v.replace('%', '=').replace("+", " "), 'UTF-8')
+            val_decode_str = decodestring(val).decode('UTF-8')
+            new_data[k] = val_decode_str
+        return new_data
